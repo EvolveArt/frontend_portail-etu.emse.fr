7f2c9d3c40e422ba3f1a9997c39fc2382edfe2b0
EvolveArt <artevolve@yahoo.com>
2021-03-05 18:48:49 +0100

    fix: prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   casLoginUrl: 'https://cas.emse.fr',
-  apiLoginUrl: 'https://portail-etu.emse.fr/api/login',
-  apiUrl: 'https://portail-etu.emse.fr/api/api',
+  apiLoginUrl: 'https://portail-etu.emse.fr/ismin/api/login',
+  apiUrl: 'https://portail-etu.emse.fr/ismin/api/api',
   apiUri: 'api',
   apiSuffix: '/api/api',
-  imgUrl: 'https://portail-etu.emse.fr/api/img',
-  excelUrl: 'https://portail-etu.emse.fr/api/excel',
-  home: 'https://portail-etu.emse.fr/',
+  imgUrl: 'https://portail-etu.emse.fr/ismin/api/img',
+  excelUrl: 'https://portail-etu.emse.fr/ismin/api/excel',
+  home: 'https://portail-etu.emse.fr/ismin/',
   production: true,
 };
